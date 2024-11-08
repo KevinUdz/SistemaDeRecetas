1021c1715541a568943bcbe89fb30bebcf775610
KevinUdz <kevinuruel22@gmail.com>
2024-11-08 05:08:25 -0600

    Delete ingrediente.cpp

--- ingrediente.cpp
+++
@@ -1,8 +0,0 @@
-// ingrediente.cpp
-#include "ingrediente.h"
-
-Ingrediente::Ingrediente(const std::string& nombre) : nombre(nombre) {}
-
-std::string Ingrediente::getNombre() const {
-    return nombre;
-}
